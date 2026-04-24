80a85463ee38218753e3aac406a604ba17999fe5
arnulfosalazar <arnulfosalazar2001@gmail.com>
2026-04-24 02:22:18 -0500

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+std::wstring tetromino[7];
+
+int main() {
+	//create assets
+	tetromino[0].append(L"..X.");
+	tetromino[0].append(L"..X.");
+	tetromino[0].append(L"..X.");
+	tetromino[0].append(L"..X.");
+
+	return 0;
+}
